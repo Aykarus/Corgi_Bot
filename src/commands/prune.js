ae453ce21049f1c22673202c0575097ac1b89e5c
Chester Cari <chester_cari@outlook.com>
2020-11-10 00:52:39 -0500

    added dice roll

--- src/commands/prune.js
+++ src/commands/prune.js
@@ -7,11 +7,10 @@ module.exports = {
 
         if (isNaN(amount)) {
             return message.reply('that doesn\'t seem to be a valid number.');
-        } else if (amount < 1 || amount > 99) {
+        } else if (amount < 1 || amount >= 99) {
             return message.reply('you need to input a number between 1 and 99.');
         }
 
-        // ...
         message.channel.bulkDelete(amount, true).catch(err => {
             console.error(err);
             message.channel.send('there was an error trying to prune messages in this channel!');
---
+++ src/commands/roll-die.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'roll-die',
+    description: 'roll random number between 1-6',
+    cooldown: 5,
+    execute(message, args) {
+        var roll = Math.floor(Math.random() * 6) + 1;
+        message.channel.send(`${message.author} rolled a ${roll}`);
+    },
+};
